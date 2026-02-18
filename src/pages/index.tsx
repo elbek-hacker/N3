093faae2794e3f0a3f3014747915ed06a92baf70
Elbek <elbek-hacker@gmail.com>
2026-02-18 13:05:16 +0500

    adminProject start

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import LoginHome from "./Auth/Home";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+
+import Category from "./Dashboard/Category";
+import DashboardHome from "./Dashboard/Home";
+import Products from "./Dashboard/Products";
+import Users from "./Dashboard/Users";
+
+export { LoginHome, Login, Register, DashboardHome, Products, Category, Users}
